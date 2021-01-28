a9d02a3b19f47d231b39f75db24d7e4e348dc1b5
DoQuocBinh <binhdq@fpt.edu.vn>
2021-01-28 14:07:18 +0700

    aa

--- day 4/server.js
+++ day 4/server.js
@@ -4,8 +4,25 @@ var app = express()
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+var fs = require('fs')
+var fileName = 'notes.txt'
+app.post('/doAdd',(req,res)=>{
+    //get content from textbox txtNote
+    let note = req.body.txtNote;
+    //append the content to end of the file notes.txt
+    fs.appendFileSync(fileName,note +'\n','utf8')
+    //go to the url '/' which means index.html
+    res.redirect('/');
+})
+
 app.get('/',(req,res)=>{
     res.sendFile('index.html');
+})
+
+app.get('/new',(req,res)=>{
+    res.sendFile(publicDir + '/newNote.html');
 })
 
 var PORT = process.env.PORT || 5000
