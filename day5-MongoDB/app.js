ea122e9d63bf7e7aff711b04748fd7637116ee42
Binh Do <binhdq@fe.edu.vn>
2021-03-02 16:57:15 +0700

    mongodb

--- day5-MongoDB/app.js
+++ day5-MongoDB/app.js
@@ -1,6 +1,11 @@
 var express = require('express')
 var app = express()
 
+var MongoClient = require('mongodb').MongoClient;
+
+//var url = 'mongodb://localhost:27017';
+var url =  "mongodb+srv://tommy:123456abc@cluster0.lkrga.mongodb.net/test";
+
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
@@ -9,8 +14,11 @@ app.set('view engine','hbs')
 
 var bodyParser = require("body-parser");
 
-app.get('/',(req,res)=>{
-    res.render('home')
+app.get('/',async (req,res)=>{
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("MyDatabase");
+    let results = await dbo.collection("products").find({}).toArray();
+    res.render('home',{model:results})
 })
 
 var PORT = process.env.PORT || 5000
