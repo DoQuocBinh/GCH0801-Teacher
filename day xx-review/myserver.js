29b9852d33d5dc6ecc8bcb3d16357aa75f619ffd
Binh Do <binhdq@fe.edu.vn>
2021-03-09 14:11:23 +0700

    ok4

--- day xx-review/myserver.js
+++ day xx-review/myserver.js
@@ -7,6 +7,21 @@ app.set('view engine','hbs')
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }))
 var url =  "mongodb+srv://tommy:123456abc@cluster0.lkrga.mongodb.net/test";
+var MongoClient = require('mongodb').MongoClient;
+
+app.post('/doLogin',(req,res)=>{
+    let nameInput = req.body.txtName;
+    let passInput = req.body.txtPassword;
+    if(nameInput != 'admin' || passInput !='admin'){
+        res.render('login',{errorMsg: "Username va mat khau incorrect!"})
+    }else{
+       res.redirect('/')
+    }
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
 
 app.get('/', async (req,res)=>{
     let client= await MongoClient.connect(url);
