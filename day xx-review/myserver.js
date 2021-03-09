e1f8c32ea4ee0317f281ab9461906d634e2880a8
Binh Do <binhdq@fe.edu.vn>
2021-03-09 13:41:11 +0700

    ok2

--- day xx-review/myserver.js
+++ day xx-review/myserver.js
@@ -1,11 +1,16 @@
 var express = require('express')
 var hbs = require('hbs')
 
+var app = express()
 app.set('view engine','hbs')
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }))
 
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 
 const PORT = process.env.PORT || 5000
